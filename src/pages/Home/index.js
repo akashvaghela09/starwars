0262a66f909ed4187c910ce1fb8aa69ea7785a11
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-30 04:59:08 +0530

    key navigation added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,12 +10,13 @@ function HomePage() {
   const [query, setQuery] = useState("")
   const loading = useSelector(state => state.app.isLoading)
   const data = useSelector(state => state.app.data)
+  const [active, setActive] = useState(0);
 
   const handleQuery = (event) => {
     dispatch(getRequest())
     let string = event.target.value
     setQuery(string)
- 
+    
     debouncedReq()
   }
 
@@ -24,6 +25,7 @@ function HomePage() {
       .then((res) => {
         let personList = [...res.data.results]
         console.log(personList);
+        console.log(personList[active]);
         if(query.length === 1){
           dispatch(getSuccess([]))
         } else {
@@ -47,6 +49,37 @@ function HomePage() {
 
   const debouncedReq = debounce(getData, 2000)
 
+  const handleResult = (e) => {
+
+    switch (e.keyCode) {
+      case 40: {
+        if (active >= data.length - 1) {
+          setActive(0);
+        } else if (active < data.length - 1) {
+          setActive((prev) => prev + 1);
+        }
+        break;
+      }
+      case 38: {
+        if (active === 1) {
+          setActive(0);
+        } else if (active <= 0) {
+          setActive(data.length - 1);
+        } else {
+          setActive((prev) => prev - 1);
+        }
+        break;
+      }
+      case 13: {
+        // onEnter
+        alert(data[active].name);
+        break;
+      }
+      default: {
+        return;
+      }
+    }
+  }
   return (
     <div>
       {loading && <h2>Loading</h2>}
@@ -58,12 +91,15 @@ function HomePage() {
         onChange={handleQuery} 
         className="search-input" 
         placeholder="Search by name" 
+        onKeyUp={handleResult}
       />
       {
-        data && data.map((el) => 
-          <h2>Name: {el.name}</h2>
+        data && data.map((el, index) => 
+          active === index ? <h2 style={{background: "green", padding: "10px", margin: "10px"}}>Name: {el.name}</h2>
+          : <h2 style={{background: "white", padding: "10px", margin: "10px"}}>Name: {el.name}</h2>
         )
       }
+     
     </div>
   );
 }
