b6f689c1feb19123a2e1b70381261c40d497a994
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-30 12:19:46 +0530

    loader changed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,11 @@ import styles from "../Styles/Loader.module.css"
 
 const Loader = () => {
     return (
-        <div className={styles.loader}></div>
+        <div className={styles.loadingio_spinner_spinner_sc52rnx41ih}>
+            <div className={styles.ldio_rakypf9kp}>
+                <div/><div/><div/><div/><div/><div/><div/><div/><div/><div/><div/><div/>
+            </div>
+        </div>
     )
 }
 
