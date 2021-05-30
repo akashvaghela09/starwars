b54def11772aba92ac5ea9cced07520149a7f744
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-30 12:19:14 +0530

    redirect added

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import styles from "../Styles/ResultCard.module.css"
+import { useHistory } from 'react-router';
 
 const ResultCard = ({data, active}) => {
     const { name, birth_year, gender} = data
+    const history = useHistory()
+
+    const handleRedirect = () => {
+        return history.push(`/person/${name.split(" ").join("_")}`) 
+    }
 
     return (
         <>
         {!active ?
-        <div className={styles.cardDiv} >
+        <div onClick={handleRedirect} className={styles.cardDiv} >
             <p><b className={styles.name}>{name}</b></p>
             <p>{birth_year}</p>
             <p className={styles.gender}>{gender}</p>
         </div> :
-        <div className={styles.lightCard}>
+        <div onClick={handleRedirect} className={styles.lightCard}>
             <p><b className={styles.name}>{name}</b></p>
             <p>{birth_year}</p>
             <p className={styles.gender}>{gender}</p>
-        </div>}</>
+        </div>}
+        </>
     )
 }
 
